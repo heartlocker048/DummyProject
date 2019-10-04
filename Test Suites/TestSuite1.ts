f4a046bc527605aa537b66e9b57631fc2c3aa242
parvinder <singh.parvinder1100@gmail.com>
2019-10-04 06:53:48 +0530

    test suite added

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>6450c833-6234-4925-a81f-ad6e1f3c9858</testSuiteGuid>
+   <testCaseLink>
+      <guid>0d835055-0255-4f51-a64a-559183bb5ed7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
